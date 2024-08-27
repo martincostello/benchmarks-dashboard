193e2df2c4f33a60b7c27d550cda5df0256961fd
martincostello <martin@martincostello.com>
2024-08-27 13:28:38 +0100

    Add build metadata
    Add metadata about the deployed version of the application to the footer.

---
+++ src/Dashboard/GitMetadata.cs
@@ -0,0 +1,51 @@
+﻿// Copyright (c) Martin Costello, 2024. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+using System.Reflection;
+
+namespace MartinCostello.Benchmarks;
+
+/// <summary>
+/// A class containing Git metadata for the application.
+/// </summary>
+public static class GitMetadata
+{
+    /// <summary>
+    /// Gets the ID of the build of the application.
+    /// </summary>
+    public static string BuildId { get; } = GetMetadataValue("BuildId", string.Empty);
+
+    /// <summary>
+    /// Gets the name of the branch of the application was built from.
+    /// </summary>
+    public static string Branch { get; } = GetMetadataValue("CommitBranch", "Unknown");
+
+    /// <summary>
+    /// Gets the Git SHA of the commit of the application was built from.
+    /// </summary>
+    public static string Commit { get; } = GetMetadataValue("CommitHash", "HEAD");
+
+    /// <summary>
+    /// Gets the URL of the GitHub repository.
+    /// </summary>
+    public static string RepositoryUrl { get; } = GetMetadataValue("RepositoryUrl", "https://github.com/martincostello/benchmarks-dashboard");
+
+    /// <summary>
+    /// Gets the date and time the application was built.
+    /// </summary>
+    public static DateTimeOffset Timestamp { get; } = DateTimeOffset.Parse(GetMetadataValue("BuildTimestamp", DateTimeOffset.UtcNow.ToString("u", CultureInfo.InvariantCulture)), CultureInfo.InvariantCulture);
+
+    /// <summary>
+    /// Gets the version number of the application.
+    /// </summary>
+    public static string Version { get; } = typeof(GitMetadata).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()!.InformationalVersion;
+
+    private static string GetMetadataValue(string name, string defaultValue)
+    {
+        return typeof(GitMetadata).Assembly
+            .GetCustomAttributes<AssemblyMetadataAttribute>()
+            .Where((p) => string.Equals(p.Key, name, StringComparison.Ordinal))
+            .Select((p) => p.Value)
+            .FirstOrDefault() ?? defaultValue;
+    }
+}
