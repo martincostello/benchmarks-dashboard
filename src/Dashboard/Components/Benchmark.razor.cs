8fda4e819e55f7c27f41507a2e98342d67d35f24
martincostello <martin@martincostello.com>
2024-09-02 16:53:08 +0100

    Add code-behind for Razor files
    Move the majority of the C# for the razor files out of `@code` regions and into class files instead.

---
+++ src/Dashboard/Components/Benchmark.razor.cs
@@ -0,0 +1,77 @@
+﻿// Copyright (c) Martin Costello, 2024. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+using MartinCostello.Benchmarks.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace MartinCostello.Benchmarks.Components;
+
+public partial class Benchmark
+{
+    /// <summary>
+    /// Gets the benchmark name.
+    /// </summary>
+    [Parameter]
+    public required string Name { get; init; }
+
+    /// <summary>
+    /// Gets the name of the associated benchmark suite.
+    /// </summary>
+    [Parameter]
+    public required string Suite { get; init; }
+
+    /// <summary>
+    /// Gets the benchmark items.
+    /// </summary>
+    [Parameter]
+    public required IList<BenchmarkItem> Items { get; init; }
+
+    /// <summary>
+    /// Gets the <see cref="IJSRuntime"/> to use.
+    /// </summary>
+    [Inject]
+    public required IJSRuntime JS { get; init; }
+
+    private string Id => $"{Suite}-{Name}";
+
+    private string ChartId => $"{Suite}-{Name}-chart";
+
+    /// <inheritdoc/>
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (!Options.Value.DataSetColors.TryGetValue("Memory", out var memoryColor) ||
+            memoryColor is not { Length: > 0 })
+        {
+            memoryColor = "#e34c26";
+        }
+
+        if (!Options.Value.DataSetColors.TryGetValue("Time", out var timeColor) ||
+            timeColor is not { Length: > 0 })
+        {
+            timeColor = "#178600";
+        }
+
+        var options = System.Text.Json.JsonSerializer.Serialize(new
+        {
+            colors = new
+            {
+                memory = memoryColor,
+                time = timeColor,
+            },
+            dataset = Items,
+            name = Name,
+            suiteName = Suite,
+        });
+
+        await JS.InvokeVoidAsync("renderChart", [ChartId, options]);
+
+        if (firstRender)
+        {
+            await JS.InvokeVoidAsync("scrollToActiveChart", []);
+        }
+    }
+
+    /// <inheritdoc/>
+    protected override bool ShouldRender() => true;
+}
---
+++ src/Dashboard/Layout/Navbar.razor.cs
@@ -0,0 +1,52 @@
+﻿// Copyright (c) Martin Costello, 2024. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.Benchmarks.Layout;
+
+public sealed partial class Navbar : IAsyncDisposable
+{
+    private string? _dataRepoUrl;
+
+    /// <summary>
+    /// Gets the dashboard options.
+    /// </summary>
+    private DashboardOptions Dashboard => Options.Value;
+
+    /// <inheritdoc/>
+    public ValueTask DisposeAsync()
+    {
+        GitHubService.OnUserChanged -= OnUserChanged;
+        return ValueTask.CompletedTask;
+    }
+
+    /// <inheritdoc/>
+    protected override async Task OnInitializedAsync()
+    {
+        var options = Options.Value;
+
+        var uriBuilder = new UriBuilder(options.GitHubServerUrl)
+        {
+            Path = $"{options.RepositoryOwner}/{options.RepositoryName}",
+        };
+
+        _dataRepoUrl = uriBuilder.Uri.ToString();
+
+        if (GitHubService.HasToken)
+        {
+            await GitHubService.VerifyTokenAsync();
+        }
+
+        GitHubService.OnUserChanged += OnUserChanged;
+    }
+
+    private void OnUserChanged(object? sender, GitHubUserChangedEventArgs args)
+        => StateHasChanged();
+
+    private async Task SignOutAsync()
+    {
+        await GitHubService.SignOutAsync();
+
+        var uri = Options.Value.IsGitHubEnterprise ? Routes.Token : Routes.Home;
+        Navigation.NavigateTo(uri);
+    }
+}
