4e5ff98c06794f9704c8143e56745fb3e27bd4fb
martincostello <martin@martincostello.com>
2024-09-03 09:23:27 +0100

    Move page models
    Move page models to their own namespace away from Pages.

--- tests/Dashboard.Tests/DashboardTests.cs
+++ tests/Dashboard.Tests/DashboardTests.cs
@@ -2,7 +2,7 @@
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
 using System.Runtime.CompilerServices;
-using MartinCostello.Benchmarks.Pages;
+using MartinCostello.Benchmarks.PageModels;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Playwright;
 
