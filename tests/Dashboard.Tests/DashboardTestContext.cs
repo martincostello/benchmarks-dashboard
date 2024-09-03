bee48fb5827e365ebe9af39b05e51b8b3f97aacd
martincostello <martin@martincostello.com>
2024-09-03 16:51:49 +0100

    Add tests for Navbar
    Add bUnit tests for the Navbar component.

--- tests/Dashboard.Tests/DashboardTestContext.cs
+++ tests/Dashboard.Tests/DashboardTestContext.cs
@@ -1,9 +1,11 @@
 ﻿// Copyright (c) Martin Costello, 2024. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
+using System.Net;
 using Blazored.LocalStorage;
 using Bunit;
 using JustEat.HttpClientInterception;
+using MartinCostello.Benchmarks.Models;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace MartinCostello.Benchmarks;
@@ -18,6 +20,7 @@ public abstract class DashboardTestContext : TestContext
         Options = new()
         {
             BenchmarkFileName = "data.json",
+            BrandIcons = ["fa-solid fa-rocket"],
             GitHubApiUrl = new("https://api.github.local"),
             GitHubApiVersion = "2022-11-28",
             GitHubClientId = "dkd73mfo9ASgjsfnhJD8",
@@ -48,4 +51,44 @@ public abstract class DashboardTestContext : TestContext
     protected DashboardOptions Options { get; }
 
     private LocalStorage LocalStorage { get; }
+
+    protected void UsingGitHubDotCom()
+    {
+        Options.GitHubApiUrl = new("https://api.github.com");
+        Options.GitHubDataUrl = new("https://raw.githubusercontent.com");
+        Options.GitHubServerUrl = new("https://github.com");
+    }
+
+    protected void WithBenchmarks(string repository, string branch)
+    {
+        RegisterResponse($"https://api.github.local/repos/{Options.RepositoryOwner}/{repository}", $"{repository}-repo");
+        RegisterResponse($"https://api.github.local/repos/{Options.RepositoryOwner}/{repository}/branches", $"{repository}-branches");
+        RegisterResponse($"https://api.github.local/repos/{Options.RepositoryOwner}/{Options.RepositoryName}/contents/{repository}/data.json?ref={branch}", $"{repository}-{branch}");
+    }
+
+    protected void WithDeviceCode(GitHubDeviceCode deviceCode)
+    {
+        var builder = new HttpRequestInterceptionBuilder()
+            .ForPost()
+            .ForUrl($"https://github.local/login/device/code?client_id={Options.GitHubClientId}&scope=public_repo")
+            .WithJsonContent(deviceCode);
+
+        builder.RegisterWith(Interceptor);
+    }
+
+    protected async Task WithValidAccessToken(string accessToken = "VALID_GITHUB_ACCESS_TOKEN")
+    {
+        await Services.GetRequiredService<GitHubTokenStore>().StoreTokenAsync(accessToken);
+        RegisterResponse("https://api.github.local/user", "user-valid-token");
+    }
+
+    protected void RegisterResponse(string url, string name, HttpStatusCode statusCode = HttpStatusCode.OK)
+    {
+        var builder = new HttpRequestInterceptionBuilder()
+            .ForUrl(url)
+            .WithStatus(statusCode)
+            .WithContentStream(() => File.OpenRead(Path.Combine(".", "Responses", $"{name}.json")));
+
+        builder.RegisterWith(Interceptor);
+    }
 }
---
+++ tests/Dashboard.Tests/Layouts/NavbarTests.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Martin Costello, 2024. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+using Bunit;
+using MartinCostello.Benchmarks.Layout;
+
+namespace MartinCostello.Benchmarks.Layouts;
+
+public class NavbarTests : DashboardTestContext
+{
+    [Fact]
+    public async Task Component_Renders_When_Signed_In()
+    {
+        // Arrange
+        await WithValidAccessToken();
+
+        // Act
+        var actual = RenderComponent<Navbar>();
+
+        // Assert
+        actual.WaitForAssertion(
+            () => actual.Find("[id='user-name']").TextContent.ShouldContain("speedy"),
+            TimeSpan.FromSeconds(2));
+    }
+
+    [Fact]
+    public void Component_Renders_When_Signed_Out()
+    {
+        // Arrange
+        UsingGitHubDotCom();
+
+        // Act
+        var actual = RenderComponent<Navbar>();
+
+        // Assert
+        actual.WaitForAssertion(
+            () => actual.Find("[id='sign-in']").TextContent.ShouldContain("Sign in"),
+            TimeSpan.FromSeconds(2));
+    }
+}
