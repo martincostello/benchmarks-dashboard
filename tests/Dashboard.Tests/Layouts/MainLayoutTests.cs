bf94965253a35208ea27c8ea08363551453d439c
martincostello <martin@martincostello.com>
2024-09-03 16:57:17 +0100

    Add test for MainLayout
    Add a bUnit test for `MainLayout`.

---
+++ tests/Dashboard.Tests/Layouts/MainLayoutTests.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Martin Costello, 2024. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+using Bunit;
+using MartinCostello.Benchmarks.Layout;
+
+namespace MartinCostello.Benchmarks.Layouts;
+
+public class MainLayoutTests : DashboardTestContext
+{
+    [Fact]
+    public void Component_Renders()
+    {
+        // Arrange
+        JSInterop.SetupVoid("configureToolTips", (_) => true);
+
+        // Act
+        var actual = RenderComponent<MainLayout>();
+
+        // Assert
+        actual.Find(".blazor");
+    }
+}
