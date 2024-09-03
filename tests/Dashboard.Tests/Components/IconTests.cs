2fbfafa3d3aa37d1ea83c4122763d4acedd761b8
martincostello <martin@martincostello.com>
2024-09-03 14:40:19 +0100

    Add component tests
    Add bUnit tests for some components.

---
+++ tests/Dashboard.Tests/Components/IconTests.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) Martin Costello, 2024. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+using Bunit;
+
+namespace MartinCostello.Benchmarks.Components;
+
+public class IconTests : DashboardTestContext
+{
+    [Theory]
+    [InlineData("fa-brands fa-github", null, false, false, false, "fa-brands fa-github")]
+    [InlineData("fa-brands fa-github", "dark", false, false, false, "fa-brands fa-github text-dark")]
+    [InlineData("fa-brands fa-github", null, true, false, false, "fa-brands fa-github fa-fw")]
+    [InlineData("fa-brands fa-github", null, false, true, false, "fa-brands fa-github fa-spin")]
+    [InlineData("fa-brands fa-github", null, false, false, true, "fa-brands fa-github fa-stack-1x")]
+    [InlineData("fa-brands fa-github", "danger", true, true, true, "fa-brands fa-github text-danger fa-fw fa-spin fa-stack-1x")]
+    public void Icon_Renders_Correctly(
+        string name,
+        string? color,
+        bool fixedWidth,
+        bool spin,
+        bool stacked,
+        string expected)
+    {
+        // Act
+        var actual = RenderComponent<Icon>((builder) =>
+        {
+            builder.Add((p) => p.Name, name)
+                   .Add((p) => p.Color, color)
+                   .Add((p) => p.FixedWidth, fixedWidth)
+                   .Add((p) => p.Spin, spin)
+                   .Add((p) => p.Stacked, stacked);
+        });
+
+        // Assert
+        actual.MarkupMatches($@"<span class=""{expected}"" aria-hidden=""true""></span>");
+    }
+}
---
+++ tests/Dashboard.Tests/DashboardTestContext.cs
@@ -0,0 +1,47 @@
+﻿// Copyright (c) Martin Costello, 2024. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+using Blazored.LocalStorage;
+using Bunit;
+using JustEat.HttpClientInterception;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MartinCostello.Benchmarks;
+
+public abstract class DashboardTestContext : TestContext
+{
+    protected DashboardTestContext()
+        : base()
+    {
+        Interceptor = new HttpClientInterceptorOptions().ThrowsOnMissingRegistration();
+        LocalStorage = new();
+        Options = new()
+        {
+            BenchmarkFileName = "data.json",
+            GitHubApiUrl = new("https://api.github.local"),
+            GitHubApiVersion = "2022-11-28",
+            GitHubClientId = "dkd73mfo9ASgjsfnhJD8",
+            GitHubServerUrl = new("https://github.local"),
+            Repositories = ["benchmarks-demo", "website"],
+            RepositoryOwner = "martincostello",
+            RepositoryName = "benchmarks",
+            TokenScopes = ["public_repo"],
+        };
+
+        Services.AddSingleton((_) => Microsoft.Extensions.Options.Options.Create(Options));
+        Services.AddSingleton((_) => Interceptor.CreateHttpClient());
+
+        Services.AddSingleton<ILocalStorageService>(LocalStorage);
+        Services.AddSingleton<ISyncLocalStorageService>(LocalStorage);
+
+        Services.AddSingleton<GitHubClient>();
+        Services.AddSingleton<GitHubService>();
+        Services.AddSingleton<GitHubTokenStore>();
+    }
+
+    protected HttpClientInterceptorOptions Interceptor { get; }
+
+    protected DashboardOptions Options { get; }
+
+    private LocalStorage LocalStorage { get; }
+}
