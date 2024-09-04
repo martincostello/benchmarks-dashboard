24c89fa2fa6475130d81fb5c129b681f7defb534
martincostello <martin@martincostello.com>
2024-09-04 17:25:47 +0100

    Configurable error bars
    - Add configuration option to allow error bars to be disabled.
    - Extend assertions on `renderChart()` arguments.

--- tests/Dashboard.Tests/Components/BenchmarkSuiteTests.cs
+++ tests/Dashboard.Tests/Components/BenchmarkSuiteTests.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) Martin Costello, 2024. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
+using System.Text.Json;
+using System.Text.Json.Nodes;
 using Bunit;
 using MartinCostello.Benchmarks.Models;
 
@@ -12,33 +14,59 @@ public class BenchmarkSuiteTests : DashboardTestContext
     public void BenchmarkSuite_Renders_Correctly()
     {
         // Arrange
-        string name = "Suite";
+        string suite = "Suite";
+
         var benchmarks = new Dictionary<string, IList<BenchmarkItem>>()
         {
             ["Benchmark1"] = [new(new(), new() { Value = 1 })],
             ["Benchmark2"] = [new(new(), new() { Value = 2 })],
         };
 
-        JSInterop.SetupVoid("renderChart", (args) => args.Arguments.Count is 2 && args.Arguments[0] is "Suite-Benchmark1-chart");
-        JSInterop.SetupVoid("renderChart", (args) => args.Arguments.Count is 2 && args.Arguments[0] is "Suite-Benchmark2-chart");
+        foreach (string benchmark in benchmarks.Keys)
+        {
+            JSInterop.SetupVoid("renderChart", (args) => RenderArgumentsAreValid($"{suite}-{benchmark}-chart", args.Arguments));
+        }
 
         // Act
         var actual = RenderComponent<BenchmarkSuite>((builder) =>
         {
-            builder.Add((p) => p.Name, name)
+            builder.Add((p) => p.Name, suite)
                    .Add((p) => p.Benchmarks, benchmarks);
         });
 
         // Assert
-        var element = actual.Find($"[id='{name}']");
+        var element = actual.Find($"[id='{suite}']");
         element.ShouldNotBeNull();
 
-        actual.Find("h2").TextContent.Trim().ShouldBe(name);
+        actual.Find("h2").TextContent.Trim().ShouldBe(suite);
 
         var charts = actual.FindAll(".benchmark-chart");
         charts.Count.ShouldBe(2);
 
         charts[0].GetAttribute("name").ShouldBe("Benchmark1");
         charts[1].GetAttribute("name").ShouldBe("Benchmark2");
+    }
+
+    private static bool RenderArgumentsAreValid(string expectedChartId, IReadOnlyList<object?> arguments)
+    {
+        if (arguments.Count is not 2 ||
+            arguments[0] is not string chartId ||
+            !string.Equals(expectedChartId, chartId, StringComparison.Ordinal) ||
+            arguments[1] is not string configString)
+        {
+            return false;
+        }
+
+        var node = JsonObject.Parse(configString);
+        var config = node.ShouldBeOfType<JsonObject>();
+
+        config.ShouldNotBeNull();
+        config["colors"].ShouldNotBeNull().GetValueKind().ShouldBe(JsonValueKind.Object);
+        config["dataset"].ShouldNotBeNull().GetValueKind().ShouldBe(JsonValueKind.Array);
+        config["errorBars"].ShouldNotBeNull().GetValue<bool>().ShouldBeTrue();
+        config["name"].ShouldNotBeNull().GetValue<string>().ShouldNotBeNullOrWhiteSpace();
+        config["suiteName"].ShouldNotBeNull().GetValue<string>().ShouldNotBeNullOrWhiteSpace();
+
+        return true;
     }
 }
